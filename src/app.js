8c8b3ee6203e3fed964f0993d3911076b8c587eb
Dmtrs Flaco Meng <tydeas.dr@gmail.com>
2020-03-23 01:44:22 +0200

    feat(interactive): Click to create route

--- src/app.js
+++ src/app.js
@@ -1,18 +1,55 @@
-const mapbox  = require('mapbox-gl/dist/mapbox-gl.js');
+const mapboxgl  = require('mapbox-gl/dist/mapbox-gl.js');
 
 const init = () => {
-  mapbox.accessToken =  '__MAPBOX_API_KEY__';
-  const map = new mapbox.Map({
+  // Setup map
+  mapboxgl.accessToken =  '__MAPBOX_API_KEY__';
+  const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10'
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [ 23.7187359, 37.9770618],
+    zoom: 14
   });
+  // disable map zoom when using scroll
+  //map.scrollZoom.disable();
 
-  //Mojave
+  // Dark mode
   const darkmode = window.matchMedia('(prefers-color-scheme: dark)');
   if(darkmode.matches) {
     map.setStyle('mapbox://styles/mapbox/dark-v10');
     darkmode.addEventListener('change', (e) => alert(JSON.stringify(e)));
   }
+
+  const coordinates = [];
+  map.on('load', () => {
+    const data = (coordinates) => ({
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+        'type': 'LineString',
+        'coordinates': coordinates
+      }
+    });
+    map.addSource('route', { 'type': 'geojson', 'data': data(coordinates) });
+    
+    map.addLayer({
+      'id': 'route',
+      'type': 'line',
+      'source': 'route',
+      'layout': {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      'paint': {
+        'line-color': '#888',
+        'line-width': 8
+      }
+    });
+
+    map.on('click', function(e) {
+      coordinates.push(e.lngLat.toArray());
+      map.getSource('route').setData(data(coordinates));
+    });
+  });
 };
 
 
